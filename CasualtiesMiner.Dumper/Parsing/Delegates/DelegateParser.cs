bfdaeff2f0130c9824425de0bc4308090f8bb41b
MelnCat <melncatuwu@gmail.com>
2026-07-15 14:55:36 -0700

    Some initial work

--- CasualtiesMiner.Dumper/Parsing/Delegates/DelegateParser.cs
+++ CasualtiesMiner.Dumper/Parsing/Delegates/DelegateParser.cs
@@ -1,4 +1,5 @@
-﻿using AngouriMath;
+﻿using System.Collections.Immutable;
+using AngouriMath;
 using TreeSitter;
 
 namespace CasualtiesMiner.Dumper.Parsing.Delegates;
@@ -13,8 +14,39 @@ internal static class DelegateParser
         using var tree = parser.Parse(string.Join("\n", lines))!;
         Query query = new Query(Language, @"(local_function_statement body: (_) @body)");
         var body = query.Execute(tree.RootNode).Captures.First().Node;
-        Entity expr = "x + sin(y)";
-        Console.WriteLine(expr);
-        Console.WriteLine($"Root node: {body.Text}");
+        ParseBlock(body);
+    }
+
+    private static void ParseBlock(Node block, Dictionary<Entity, Entity>? variables = null)
+    {
+        variables ??= [];
+        foreach (var decl in block.NamedChildren.Where(x => x.Type == "local_declaration_statement"))
+        {
+            var parsed = CapturesToDictionary(new Query(Language,
+                """
+                (local_declaration_statement 
+                                  (variable_declaration type: (predefined_type) @type (variable_declarator name: (identifier) @name (_) @content)))
+                """).Execute(decl).Captures);
+            if (parsed.Count == 0)
+            {
+                AddWarning($"Failed to parse assignment {decl.Text}");
+                continue;
+            }
+
+            var type = parsed["type"];
+            if (type.Text == "float" || type.Text == "bool")
+            {
+                variables[parsed["name"].Text] = MathParser.ToMath(parsed["content"]);
+            }
+        }
+    }
+
+    private static void AddWarning(string warning)
+    {
+    }
+
+    private static Dictionary<string, Node> CapturesToDictionary(IEnumerable<QueryCapture> captures)
+    {
+        return captures.ToDictionary(x => x.Name, x => x.Node);
     }
 }
---
+++ CasualtiesMiner.Dumper/Parsing/Delegates/MathParser.cs
@@ -0,0 +1,48 @@
+﻿using AngouriMath;
+using TreeSitter;
+
+namespace CasualtiesMiner.Dumper.Parsing.Delegates;
+
+internal static class MathParser
+{
+    public static Entity ToMath(Node node)
+    {
+        switch (node.Type)
+        {
+            case "identifier":
+                return MathS.Var(node.Text);
+            case "integer_literal":
+                return MathS.Numbers.Create(int.Parse(node.Text));
+            case "real_literal":
+                return MathS.Numbers.Create(float.Parse(node.Text.Replace("f", "")));
+            case "binary_expression":
+            {
+                var left = ToMath(node.GetChildForField("left")!);
+                var right = ToMath(node.GetChildForField("right")!);
+                var op = node.Children[1].Text;
+                return op switch
+                {
+                    "+" => left + right,
+                    "-" => left - right,
+                    "*" => left * right,
+                    "/" => left / right,
+                    _ => throw new Exception($"Unknown operator {op}")
+                };
+            }
+            case "prefix_unary_expression":
+            {
+                var op = node.Children[0].Text;
+                var value = ToMath(node.Children[1]);
+                return op switch
+                {
+                    "!" => !value,
+                    "-" => -value,
+                    "+" => +value,
+                    _ => throw new Exception($"Unknown unary operator {op}")
+                };
+            }
+            default:
+                throw new Exception($"Unknown identifier of type {node.Type}: {node.Text}");
+        }
+    }
+}
